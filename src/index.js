53820870fb35ba421d815b6335460b9663ae02ab
Nauman-555 <naumannzaib@gmail.com>
2024-04-08 21:38:04 +0500

    Custom Api and Error Handling

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import connectDB from "./db/index.js";
+
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./env",
+  // the above object may or may not be here!
+});
+
+connectDB()
+  // As we are avoiding require syntax for importing dotenv.
+  // Therefore, we are using the above config function and also
+  // such lines in package.json '-r dotenv/config --experimental-json-modules'
+
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`The Server is running on Port:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Connection failed by error:", err);
+  });
---
+++ src/utilis/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Errors {
+  constructor(
+    statusCode,
+    message = "Message not sent",
+    stack = "",
+    error = []
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    this.errors = errors;
+    this.data = null;
+    this.success = false;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+export { ApiError };
